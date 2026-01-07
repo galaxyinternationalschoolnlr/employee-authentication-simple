95a5620dda96266242608d4e4ff2005b4aaec37e
Sunku  Venkata Revanth Kumar <63785642+revanth112@users.noreply.github.com>
2026-01-08 00:44:52 +0530

    Modify employeeDatabase with new teacher entries
    Updated teacher details and added new entries.

--- config.js
+++ config.js
@@ -3,20 +3,122 @@
 
 const employeeDatabase = [
     {
-        id: "GIS-101",
-        name: "Mrs. Aruna Devi",
+        id: "101",
+        name: "Roja Rani",
         role: "Principal",
         photo: "picsdata/101.png"  // Ensure this file exists in the folder
     },
     {
-        id: "GIS-102",
-        name: "Mr. Rajesh Kumar",
+        id: "102",
+        name: "Mehtaj",
         role: "Mathematics",
         photo: "picsdata/102.png"
     },
     {
-        id: "GIS-103",
-        name: "Ms. Sarah Jones",
+        id: "103",
+        name: "Rishitha",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "104",
+        name: "Neeraja",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "105",
+        name: "Mary",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "106",
+        name: "Sri Lakshmi",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "107",
+        name: "Madhuri",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "108",
+        name: "Sandhya",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "109",
+        name: "Jaya Lakshmi",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "110",
+        name: "Kottaya",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "111",
+        name: "Kalyani",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "112",
+        name: "Srinivasu",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "113",
+        name: "Kp",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "114",
+        name: "Naveen",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "115",
+        name: "Venkateshwarlu",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "116",
+        name: "Anapurna",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "117",
+        name: "Supriya",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "118",
+        name: "Sirisha",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "119",
+        name: "Supraja",
+        role: "Science",
+        photo: "picsdata/103.png"
+    },
+    {
+        id: "120",
+        name: "Swarnadhri",
         role: "Science",
         photo: "picsdata/103.png"
     }
